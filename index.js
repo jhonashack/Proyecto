ee53f5a0fc07e3d5647dfa3cab4c485316bc1dfc
wilmerllerenaucc <wilmer.llerena@campusucc.edu.co>
2023-10-02 19:09:27 -0500

    modify index

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-const http = require ("http")
+var express = require('express');
+var app = express();
+const path = require('path'); // Import the 'path' module
 
-const fs = require("fs");//
+const PUERTO = 8000;
 
-const host = "localhost"
-const port = 8000
+app.listen(PUERTO, function(){
+	console.log('Servidor http correindo en el puerto 8000');
+});
 
-const requestListener = function (req, res) {
-    // Lee el contenido de 'index.html' de manera asíncrona.
-    fs.readFile("index.html", "utf8", (err, data) => {
-      if (err) {
-        // Maneja el error si no se puede leer el archivo.
-        res.writeHead(404);
-        res.
-       
-  end("Error: Página no encontrada problem");
-      } else {
-        // Si se pudo leer el archivo, envía su contenido como respuesta.
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      }
-    });
-  };
-  
-  
-      
-const server = http.createServer(requestListener);
-  server.listen(port, host, () => {
-    console.log("Node.js está funcionando en el puerto 8000");
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, 'index.html'));
+	console.log('Se recibio una petición get');
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/contacto", (req, res) => {
+	res.send("ruta de contacto 1");
   });
-
+  app.get("/admin", (req, res) => {
+	res.send("ruta de administracion 1");
+  });
+  app.use((req, res, next) => {
+	// res.status(404).send("Sorry cant find that!");
+	res.status(404).sendFile(__dirname + "/public/404.html");
+  });
+  app.get('/public', (req, res) => {
+    res.sendFile(path.join(__dirname, '404.html'));
+});
