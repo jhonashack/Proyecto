b9ee2b9b67486d7741053a9dfef345c2c339e9f4
wilmerllerenaucc <wilmer.llerena@campusucc.edu.co>
2023-10-05 19:13:45 -0500

    update

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 var express = require('express');
 var app = express();
-const path = require('path'); // Import the 'path' module
+const path = require('path');
 
-const PUERTO = 8000;
+const PUERTO = 80;
 
 app.listen(PUERTO, function(){
-	console.log('Servidor http correindo en el puerto 8000');
+    console.log('Servidor http corriendo en el puerto 8000');
 });
 
-app.use(express.static(path.join(__dirname, 'assets')));
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Configurar Express para servir archivos estáticos desde la carpeta "assets"
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname, 'index.html'));
-	console.log('Se recibio una petición get');
+    res.sendFile(path.join(__dirname, 'index.html'));
+    console.log('Se recibió una petición get');
 });
 
-
-app.get("/contacto", (req, res) => {
-	res.send("ruta de contacto 1");
-  });
-  app.get("/admin", (req, res) => {
-	res.send("ruta de administracion 1");
-  });
-  app.use((req, res, next) => {
-	// res.status(404).send("Sorry cant find that!");
-	res.status(404).sendFile(__dirname + "/public/404.html");
-  });
-  app.get('/public', (req, res) => {
-    res.sendFile(path.join(__dirname, '404.html'));
+// Mover la ruta de manejo de errores 404 después de la ruta para '/'
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/public/404.html");
 });
